46b7e63a16ac10f2f15807148c2b66ed34d90a83
amine <amine.mezghich@ensi-uma.tn>
2021-02-23 16:02:02 +0100

    le routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEquipeComponent } from './add-equipe/add-equipe.component';
+import { ListEquipeComponent } from './list-equipe/list-equipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "list",
+    component: ListEquipeComponent
+  },
+  {
+    path: "add",
+    component: AddEquipeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
